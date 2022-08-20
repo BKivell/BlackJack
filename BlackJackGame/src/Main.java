305d8804d16fac2c2dd631c20ac097865c07655c
Shelvin Kumar <shalvink42@gmail.com>
2022-08-20 16:24:07 +1200

    send

--- BlackJackGame/src/Main.java
+++ BlackJackGame/src/Main.java
@@ -7,11 +7,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("sdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd");
         BlackJack game = new BlackJack();
         game.startGame();
     }
 }
 
-
-// Test
